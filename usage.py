64cfe00c20bfc71e2c562f079e36df4ec85cd849
Mingxun Wang <mwang87@gmail.com>
2020-10-19 10:20:51 -0700

    adding drop down to select the data

--- usage.py
+++ usage.py
@@ -2,6 +2,7 @@ import dash_clustergrammer
 import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
+import dash_core_components as dcc
 import pandas as pd
 
 import json
@@ -43,24 +44,59 @@ def _load_toy_matrix():
 
     return json.loads(net.export_net_json())
 
+def _load_tuple_matrix():
+    # make network object and load file
+    from clustergrammer2 import Network
+    import json
+    net = Network()
+    net.load_file('data/rc_two_cats.txt')
 
-print('loading JSON Clustergrammer data...')
-network_data = _load_json()
-network_data = _load_toy_matrix()
+    # calculate clustering using default parameters
+    net.cluster()
+
+    return json.loads(net.export_net_json())
 
 print('done loading')
+network_data = _load_json()
 
 app.layout = html.Div([
     html.Link(
         rel='stylesheet',
         href='./static/custom.css'
     ),
-    dash_clustergrammer.DashClustergrammer(
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=[
+            {'label': 'JSON', 'value': 'JSON'},
+            {'label': 'Simple Matrix', 'value': 'MATRIX'},
+            {'label': 'Tuple Matrix', 'value': 'TUPLEMATRIX'}
+        ],
+        value='JSON'
+    ),
+    html.Div(id="cgram-component")
+])
+
+
+@app.callback(
+    [Output('cgram-component', 'children')],
+    [Input('demo-dropdown', 'value')])
+def update_output(value):
+    
+    print('loading JSON Clustergrammer data...')
+    if value == "JSON":
+        network_data = _load_json()
+    if value == "MATRIX":
+        network_data = _load_toy_matrix()
+    if value == "TUPLEMATRIX":
+        network_data = _load_tuple_matrix()
+
+    cgram_fig = dash_clustergrammer.DashClustergrammer(
         id='cgram-component',
         label='Clustergrammer Dash Component',
         network_data=network_data,
     )
-])
+
+    return [[cgram_fig]]
 
 @app.server.route('/static/<path>')
 def static_file(path):
